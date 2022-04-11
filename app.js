cd34906066a9873f1cec3d2c699b184895bffef1
Ravi Gurav <ravi.gurav@bnt.local>
2022-04-11 12:48:57 +0530

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const app = express();
 const port = process.env.PORT || 3000 ;
 app.get('/' ,(req,res)=>{
-    res.send('welcome to the home page')
+    res.send('updated!')
 })
 
 app.listen(port , ()=> console.log('server start on port ' + port));       
